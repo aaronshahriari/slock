d7e1cbfe7a0307045bae06cfe7b70d905121a342
aaronshahriari <aaronshahriari@gmail.com>
2024-08-25 11:28:16 -0400

    updated with date time, updates every 5 seconds

--- config.h
+++ config.h
@@ -2,6 +2,13 @@
 static const char *user  = "nobody";
 static const char *group = "nogroup";
 
+/*Font settings for the time text*/
+static const float textsize=45.0;
+static const char* textfamily="serif";
+static const double textcolorred=255;
+static const double textcolorgreen=255;
+static const double textcolorblue=255;
+
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#fcba03",   /* during input */
